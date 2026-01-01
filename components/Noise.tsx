52399d012217045b7bab514e10d67e04b117e521
Aanchal Yadav <yadavaanchal2005@gmail.com>
2026-01-01 23:20:55 +0530

    Create Noise.tsx

---
+++ components/Noise.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Noise() {
+  return (
+    <div
+      className="pointer-events-none fixed inset-0 z-50 opacity-[0.035]"
+      style={{
+        backgroundImage:
+          "url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGZpbHRlciBpZD0ibm9pc2UiPjxmZVR1cmJ1bGVuY2UgdHlwZT0iZnJhY3RhbE5vaXNlIiBiYXNlRnJlcXVlbmN5PSIwLjgiIHN0aXRjaFRpbGVzPSJzdGl0Y2giLz48L2ZpbHRlcj48cmVjdCB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgZmlsdGVyPSJ1cmwoI25vaXNlKSIvPjwvc3ZnPg==)",
+      }}
+    />
+  );
+}
